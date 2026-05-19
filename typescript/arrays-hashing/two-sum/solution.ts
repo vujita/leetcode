fe624db61f59ad175c5f40c563632db24275d869
Vu Nguyen <shardphoneix@gmail.com>
2026-05-19 02:25:02 -0700

    two sum sol

--- typescript/arrays-hashing/two-sum/solution.ts
+++ typescript/arrays-hashing/two-sum/solution.ts
@@ -1,25 +1,20 @@
 // LeetCode:  https://leetcode.com/problems/two-sum/
 // NeetCode:  https://neetcode.io/problems/two-sum
 
-import { trace } from '../../_utils/trace.js';
 
 export function twoSum(nums: number[], target: number): number[] {
-  const seen = new Map<number, number>();
-
-  for (let i = 0; i < nums.length; i++) {
-    const complement = target - nums[i];
-
-    trace.step(`i=${i}  val=${nums[i]}  need=${complement}`);
-    trace.array(nums, { i });
-    trace.map(seen);
-
-    if (seen.has(complement)) {
-      trace.step(`found! indices [${seen.get(complement)}, ${i}]`);
-      return [seen.get(complement)!, i];
-    }
-
-    seen.set(nums[i], i);
+  if (nums.length === 0) {
+    return []
   }
-
-  return [];
+  // number -> index map
+  const indexMap = new Map<number, number>()
+  for (let i = 0; i < nums.length; i++) {
+    const n = nums[i];
+    const complementIndex = indexMap.get(target - n)
+    if (complementIndex !== undefined) {
+      return [i, complementIndex]
+    }
+    indexMap.set(n, i)
+  }
+  return []
 }
